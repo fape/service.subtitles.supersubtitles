fc249034028523ac254b3ba20e03dc882b6f619d
fape <fape88@gmail.com>
2014-05-03 16:54:02 +0200

    handle more exceptions

--- service.py
+++ service.py
@@ -156,10 +156,12 @@ def send_request(params):
         return urllib2.urlopen(request)
     except urllib2.HTTPError as e:
         errorlog("HTTP Error: %s, %s" % (e.code, url))
-        return None
     except urllib2.URLError as e:
         errorlog("URL Error %s, %s" % (e.reason, url))
-        return None
+    except Exception as e:
+        errorlog("Unexpected exception: %s" % e.message)
+
+    return None
 
 
 def query_data(params):
@@ -167,8 +169,10 @@ def query_data(params):
     if response:
         try:
             return json.load(response, 'utf-8')
-        except json.JSONDecodeError as e:
-            errorlog(e.message)
+        except ValueError as e:
+            errorlog("Json Decode Error: %s" % e.message)
+        except Exception as e:
+            errorlog("Unexpected exception: %s" % e.message)
     return None
 
 
